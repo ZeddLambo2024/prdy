1f17706e422b58c1cfb51af58634e93aa311fd7f
Zac <zacmahoneycampbell@gmail.com>
2018-09-20 00:54:36 -0400

    push

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,7 @@
 class DocsController < ApplicationController
   before_action :set_doc, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /docs
   # GET /docs.json
   def index
@@ -12,9 +13,8 @@ class DocsController < ApplicationController
   def show
   end
 
-  # GET /docs/new
-  def new
-    @doc = Doc.new
+   def new
+    @doc= current_user.docs.build
   end
 
   # GET /docs/1/edit
@@ -24,12 +24,12 @@ class DocsController < ApplicationController
   # POST /docs
   # POST /docs.json
   def create
-    @doc = Doc.new(doc_params)
-
+    
+    @doc = current_user.docs.new(doc_params)
     respond_to do |format|
       if @doc.save
-        format.html { redirect_to @doc, notice: 'PRD was successfully created.' }
-        format.json { render :show, status: :created, location: @doc }
+        format.html { redirect_to docs_url, notice: 'PRD was successfully created.' }
+        
       else
         format.html { render :new }
         format.json { render json: @doc.errors, status: :unprocessable_entity }
@@ -42,11 +42,11 @@ class DocsController < ApplicationController
   def update
     respond_to do |format|
       if @doc.update(doc_params)
-        format.html { redirect_to @doc, notice: 'PRD was successfully updated.' }
-        format.json { render :show, status: :ok, location: @doc }
+        format.html { redirect_to docs_url, notice: 'PRD was successfully updated.' }
       else
         format.html { render :edit }
         format.json { render json: @doc.errors, status: :unprocessable_entity }
+        redirect_to docs_url
       end
     end
   end
@@ -54,17 +54,18 @@ class DocsController < ApplicationController
   # DELETE /docs/1
   # DELETE /docs/1.json
   def destroy
-    @doc.destroy
-    respond_to do |format|
-      format.html { redirect_to docs_url, notice: 'PRD was successfully deleted.' }
-      format.json { head :no_content }
-    end
+    @doc.DELETE
+    redirect_to docs_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_doc
       @doc = Doc.find(params[:id])
+    end
+def correct_user
+      @doc = current_user.docs.find_by(id: params[:id])
+      redirect_to docs_path, notice: "Not authorized to edit this PRD" if @doc.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :docs
-  devise_for :users
+  devise_for :users 
   root "pages#home"
   get "about" => "pages#about"
 
