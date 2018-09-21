d02e5e354cbede658ce8b20101910095bca06b9c
Zac <zacmahoneycampbell@gmail.com>
2018-09-21 17:15:13 -0400

    push'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-    config.assets.initialize_on_precompile = false
+  config.serve_static_files = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
