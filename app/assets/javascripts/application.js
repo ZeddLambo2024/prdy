e5ad9aabfde81956be15bc7eeb2e21842153cbb1
Zac <zacmahoneycampbell@gmail.com>
2018-09-21 18:09:48 -0400

    p

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require masonry/jquery.masonry
 //= require_tree .
-
 
